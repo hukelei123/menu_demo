fe9236899e8dbd21d50977579057e03972a5dc74
wu1g119 <wu1g@163.com>
2013-07-16 17:35:31 +0800

    初始导入

---
+++ js/demo.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+	$("#demo").treeview({
+		animated: "fast",
+		collapsed: true,
+		unique: true,
+		persist: "cookie",
+		toggle: function() {
+			window.console && console.log("%o was toggled", this);
+		}
+	});
+
+});
